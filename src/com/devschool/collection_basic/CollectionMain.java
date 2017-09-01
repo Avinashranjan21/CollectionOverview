adc9356d4dc0c483f54a30de66bc04fc1aa54855
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-09-02 00:44:18 +0530

    Initial commit

---
+++ src/com/devschool/collection_basic/CollectionMain.java
@@ -0,0 +1,11 @@
+package com.devschool.collection_basic;
+
+/**
+ * Created by avinash.ranjan on 02/09/17.
+ */
+public class CollectionMain {
+
+    public static void main(String[] args) {
+
+    }
+}
